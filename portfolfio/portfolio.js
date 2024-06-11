2a5dc86d74d34999dd1c6cb52dcae290ced58021
Adin Firoz <firozzoya6@gmail.com>
2024-06-12 01:05:53 +0530

    Task 2

--- portfolfio/portfolio.js
+++ portfolfio/portfolio.js
@@ -2,3 +2,23 @@ document.querySelector('.nav-toggle').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('active');
   });
   
+  let slideIndex = 1;
+ showSlides(slideIndex);
+
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+
+ function showSlides(n) {
+   let slides = document.getElementsByClassName("slide");
+   if (n > slides.length) {
+     slideIndex = 1;
+   }
+   if (n < 1) {
+     slideIndex = slides.length;
+   }
+   for (let i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   slides[slideIndex - 1].style.display = "block";
+ }
